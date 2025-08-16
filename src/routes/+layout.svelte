58ebf9a08faebb92a9ebb747e860cc36da77b90f
nearHaan <farhaan.nizam675@gmail.com>
2025-08-17 02:26:09 +0530

    Update UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	let activeTab: string = $state('home');
 
 	function onTabClick(newTab: string) {
+		console.log(newTab);
 		activeTab = newTab;
 	}
 </script>
@@ -13,70 +14,62 @@
 	<title>Farhaan Nizam</title>
 </svelte:head>
 
-<div class="flex flex-col min-h-screen min-w-screen bg-black p-4 text-white">
-	<header class="sticky absolute top-4 flex w-full items-center justify-center">
-		<nav class="rounded-full bg-[#ffffff15] p-1 backdrop-blur-lg">
+<div class="flex min-h-screen min-w-screen flex-col bg-black p-4 text-white">
+	<header class="absolute z-10 sticky top-4 flex w-full items-center justify-center font-title">
+		<nav class="rounded-full bg-[#ffffff15] p-[2px] backdrop-blur-lg">
 			<ul class="flex">
 				<li>
-					<div
-						class="rounded-full px-3 py-1 {activeTab == 'home'
+					<button
+						onclick={() => {
+							onTabClick('home');
+						}}
+						class="rounded-full px-4 py-0.5 {activeTab == 'home'
 							? 'bg-[#ffffff15] backdrop-blur-lg'
 							: ''}"
 					>
-						<a
-							onclick={() => {
-								onTabClick('home');
-							}}
-							href="/">Home</a
-						>
-					</div>
+						Home
+					</button>
 				</li>
 				<li>
-					<div
-						class="rounded-full px-3 py-1 {activeTab == 'about'
+					<button
+						onclick={() => {
+							onTabClick('about');
+						}}
+						class="rounded-full px-4 py-0.5 {activeTab == 'about'
 							? 'bg-[#ffffff15] backdrop-blur-lg'
 							: ''}"
 					>
-						<a
-							onclick={() => {
-								onTabClick('about');
-							}}
-							href="/">About</a
-						>
-					</div>
+						About
+					</button>
 				</li>
 				<li>
-					<div
-						class="rounded-full px-3 py-1 {activeTab == 'projects'
+					<button
+						onclick={() => {
+							onTabClick('projects');
+						}}
+						class="rounded-full px-4 py-0.5 {activeTab == 'projects'
 							? 'bg-[#ffffff15] backdrop-blur-lg'
 							: ''}"
 					>
-						<a
-							onclick={() => {
-								onTabClick('projects');
-							}}
-							href="/">Projects</a
-						>
-					</div>
+						Projects
+					</button>
 				</li>
 				<li>
-					<div
-						class="rounded-full px-3 py-1 {activeTab == 'experience'
+					<button
+						onclick={() => {
+							onTabClick('experience');
+						}}
+						class="rounded-full px-4 py-0.5 {activeTab == 'experience'
 							? 'bg-[#ffffff15] backdrop-blur-lg'
 							: ''}"
 					>
-						<a
-							onclick={() => {
-								onTabClick('experience');
-							}}
-							href="/">Experience</a
-						>
-					</div>
+						Experience
+					</button>
 				</li>
 			</ul>
 		</nav>
 	</header>
-	<main class="absolute inset-0">
+	<main class="z-0 absolute inset-0">
 		{@render children?.()}
 	</main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="relative p-10 flex items-center h-screen">
     <div class="absolute z-0 top-50 -left-10 rounded-full h-60 w-60 bg-[#ffffff10] backdrop-blur-[1px]"></div>
-    <h1 class="z-20 md:ml-10 lg:ml-20 font-bold font-title text-[90px] text-[#dedede]">farhaan<br/>nizam.</h1>
-    <div class="absolute z-30 left-70 rounded-full h-120 w-120 bg-[#ffffff10] backdrop-blur-[1px]"></div>
+    <h1 class="z-10 md:ml-10 lg:ml-20 font-bold font-title text-[90px] text-[#dedede]">farhaan<br/>nizam.</h1>
+    <div class="absolute z-20 left-70 rounded-full h-120 w-120 bg-[#ffffff10] backdrop-blur-[1px]"></div>
 </div>
