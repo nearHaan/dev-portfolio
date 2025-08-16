91d299c93dc7c89f4d008ee31a88fc3e22c53203
nearHaan <farhaan.nizam675@gmail.com>
2025-08-17 02:02:26 +0530

    Add mono font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <div class="p-10 flex items-center h-screen">
-    <h1 class="font-mono text-[75px] text-[#dedede]">farhaan<br/>nizam.</h1>
+    <h1 class="font-title text-[75px] text-[#dedede]">farhaan<br/>nizam.</h1>
 </div>
