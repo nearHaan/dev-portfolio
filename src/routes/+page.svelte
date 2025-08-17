e79ce13cda62f774e5b5fd3a27379e45043e5afe
nearHaan <farhaan.nizam675@gmail.com>
2025-08-17 09:01:42 +0530

    UI Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="relative p-10 flex items-center h-screen">
-    <div class="absolute z-0 top-50 -left-10 rounded-full h-60 w-60 bg-[#ffffff10] backdrop-blur-[1px]"></div>
+    <div class="absolute z-20 top-40 -left-20 rounded-full h-60 w-60 bg-[#ffffff10] border-1 border-[#ffffff30] inset-shadow-2xs inset-shadow-white backdrop-blur-[1px]"></div>
     <h1 class="z-10 md:ml-10 lg:ml-20 font-bold font-title text-[90px] text-[#dedede]">farhaan<br/>nizam.</h1>
-    <div class="absolute z-20 left-70 rounded-full h-120 w-120 bg-[#ffffff10] backdrop-blur-[1px]"></div>
+    <div class="absolute z-20 left-70 rounded-full h-120 w-120 bg-[#ffffff10] border-1 border-[#ffffff30] inset-shadow-2xs inset-shadow-white backdrop-blur-[1px]"></div>
 </div>
